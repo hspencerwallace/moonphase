8f928b03a2680e52b20202d0ed7bae5275f4fb8c
hspencerwallace <>
2020-01-28 11:38:19 -0800

    added assignment in comment to sketch

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
-
+// The function should take a arguments/parameters for 
+// size, color, and phase. You should be able to draw a 
+// number of moons with different phases, sizes and colors 
+// within your draw() function. Phase should be a number 
+// from 0 - 30, where 0 is a new moon, 15 is a full moon, 
+// and 30 is an almost new moon. 
 
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(windowWidth, windowHeight);
 
 
 
 }
-
-let noiseScale=0.02;
 
 function draw() {
   background(0);
@@ -15,4 +18,12 @@ function draw() {
 
 ellipse(200, 200, 100, 100)
 
+noFill();
+    translate(300, 300);
+    for (let x = 420; x >= 40; x = x / 1.08) {
+        rotate(radians(frameCount / 2));
+        noStroke();
+        fill(200, 40);
+        rect(0, 0, x, x);
+      }
 }
