fb3a48ecfdcb570543a058c109c9fee5d42d92b2
hspencerwallace <>
2020-02-11 11:57:02 -0800

    back to bezier blocking strategy, ellipse gemoetry is commented out at bottom below loops, roughly drew phases 0-7 and now need to make a flipped bezier for phase 8 which is a slightly over half moon.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@
 // from 0 - 30, where 0 is a new moon, 15 is a full moon, 
 // and 30 is an almost new moon. 
 
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
@@ -13,47 +15,106 @@ function setup() {
 function draw() {
   background(0);
 
-//trying this with spheres
-push();
-// translate(50, 0, 0);
-directionalLight(250, 250, 250, 0.25, 0, -1);
- pop();
 
-  // earth
-  // push();
-  // noStroke();
-  // translate(0, -0, 200)
-  // ambientMaterial(250);
-  //   sphere(370)
-  // pop();
-
-  //moon
+//simple ellipsoid translated back so the "shadow" is in front, on the z axis
   push();
-  noStroke();
-  translate(600, 0, -800);
-  specularMaterial(250);
-    sphere(100);
+    translate(0, 0, -101)
+    ellipsoid(100, 100, 100, 70);
   pop();
 
-
-
-
-
-
-// //simple ellipsoid translated back so the "shadow" is in front, on the z axis
-//   push();
-//     translate(0, 0, -101)
-//     ellipsoid(100, 100, 100, 70);
+//bezier as "shadow" of moon overlayed onto "illuminated" shape
+ 
+//phase 0 new
+// push();
+//     fill(25, 25, 25);
+//     beginShape();
+//       vertex(-100, -150);
+//       bezierVertex(150, -75, 150, 75, -100, 150);
+//     endShape(CLOSE)
 //   pop();
 
-// //bezier as "shadow" of moon overlayed onto "illuminated" shape
-//   push();
-// 	  fill(25, 25, 25);
-// 	  beginShape();
-//   	  vertex(-100, -150);
-//   	  bezierVertex(100, -75, 100, 75, -100, 150);
-// 	  endShape(CLOSE)
-//   pop();
+//phase 1
+ // push();
+ //    fill(25, 25, 25);
+ //    beginShape();
+ //     vertex(-100, -150);
+ //     bezierVertex(126, -75, 126, 75, -100, 150);
+ //    endShape(CLOSE)
+ //   pop();
+
+  //phase 2
+  // push();
+  //   fill(25, 25, 25);
+  //   beginShape();
+  //    vertex(-100, -150);
+  //    bezierVertex(112, -75, 112, 75, -100, 150);
+  //   endShape(CLOSE)
+  //  pop();
+
+  //phase 3 waxing crescent
+  // push();
+	 //  fill(25, 25, 25);
+	 //  beginShape();
+  // 	  vertex(-100, -150);
+  // 	  bezierVertex(100, -75, 100, 75, -100, 150);
+	 //  endShape(CLOSE)
+  //  pop();
+
+    //phase 4
+
+    // push();
+    // fill(50, 25, 25);
+    // beginShape();
+    //   vertex(-90, -150);
+    //   bezierVertex(80, -75, 80, 75, -90, 150);
+    // endShape(CLOSE)
+    // pop();
+
+  //phase 5 needs another vertex to block the darkside
+
+  // push();
+  //   fill(50, 25, 25);
+  //   beginShape();
+  //     vertex(-80, -150);
+  //     bezierVertex(60, -75, 60, 75, -80, 150);
+  //   endShape(CLOSE)
+  //   pop();
+
+//phase 6
+
+    // push();
+    // fill(25, 25, 25);
+    // beginShape();
+    //   vertex(-65, -150);
+    //   bezierVertex(50, -75, 50, 75, -65, 150);
+    // endShape(CLOSE)
+    // pop();
+
+    //phase 7
+
+    // push();
+    // fill(25, 25, 25);
+    // beginShape();
+    //   vertex(-50, -150);
+    //   bezierVertex(40, -75, 40, 75, -50, 150);
+    // endShape(CLOSE)
+    // pop();
+
+    //phase 8
+
+push();
+    fill(25, 25, 25);
+    beginShape();
+      vertex(-50, -150);
+      bezierVertex(40, -75, 40, 75, -50, 150);
+    endShape(CLOSE)
+    pop();    
+
+
+
+
+
+
 
 //rotating loop of many shapes
     // push();
@@ -77,3 +138,30 @@ directionalLight(250, 250, 250, 0.25, 0, -1);
 
 
 // }
+
+
+//trying this with spheres
+// // lights();
+
+// // push();
+// // translate(0, 0, 0);
+// let dirX = (mouseX / width - 0.5) * 2;
+//   let dirY = (mouseY / height - 0.5) * 2;
+//   directionalLight(250, 250, 250, -dirX, -dirY, -1);
+//  // pop();
+
+//   // earth
+//   push();
+//   noStroke();
+//   translate(0, -0, 810)
+//   ambientMaterial(250);
+//     sphere(340)
+//   pop();
+
+//   //moon
+//   push();
+//   noStroke();
+//   translate(600, 0, -800);
+//   ambientMaterial(250);
+//     sphere(100);
+//   pop();
